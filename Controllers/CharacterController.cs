7ca35406bef897016861a3e11f0ad3b06ca25973
nathan240802 <reisyan.nathan.k@gmail.com>
2023-02-22 22:49:20 +0700

    Get and Post

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters= new List<Character>{
+            new Character(),
+            new Character{ Id=1, Name="Sam"}
+        };
+
+    [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+    [HttpGet("{id}")]
+
+        public ActionResult<List<Character>> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c=>c.Id == id));
+        }
+
+    [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+
+    }
+}
