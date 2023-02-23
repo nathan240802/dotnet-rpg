ba3c189437067236765cdf85f47cfb088ed59388
nathan240802 <reisyan.nathan.k@gmail.com>
2023-02-24 03:19:52 +0700

    Auto Mapper

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,11 @@
+namespace dotnet_rpg
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character,GetCharacterDto>();
+            CreateMap<AddCharacterDto,Character>();
+        }
+    }
+}
