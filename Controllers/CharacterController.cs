6f5ee11c26b56ec7fbafe55195288f3d52143cf6
nathan240802 <reisyan.nathan.k@gmail.com>
2023-02-23 16:19:45 +0700

    Add Services

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
@@ -6,29 +7,32 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters= new List<Character>{
-            new Character(),
-            new Character{ Id=1, Name="Sam"}
-        };
+       
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+            
+        }
 
     [HttpGet("GetAll")]
-        public ActionResult<List<Character>> Get()
+        public async Task<ActionResult<List<Character>>> Get()
         {
-            return Ok(characters);
+            return Ok(await _characterService.GetAllCharacters());
         }
 
     [HttpGet("{id}")]
 
-        public ActionResult<List<Character>> GetSingle(int id)
+        public async Task<ActionResult<Character>> GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault(c=>c.Id == id));
+            return Ok(await _characterService.GetCharacterById(id));
         }
 
     [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
         {
-            characters.Add(newCharacter);
-            return Ok(characters);
+            
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
 
     }
---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,11 @@
+namespace dotnet_rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+       Task<ServiceResponse<List<Character>>> GetAllCharacters();
+
+        Task<ServiceResponse<Character>> GetCharacterById(int id);
+
+        Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
+    }
+}
